eab2871922e8aa274dde2d7eb5d5db85ab36ea60
cacheUp <bradb26@gmail.com>
2020-02-25 18:23:10 +0300

    MVP

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,6 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +9,11 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.dll = DoublyLinkedList()
+        self.storage = {}
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +22,12 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
     def get(self, key):
-        pass
+        if key not in self.storage or self.storage[key] == None:
+            return None
+        self.set(key, self.storage[key].value)
+        return self.storage[key].value
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +39,36 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
-        pass
+        if key in self.storage:
+            node = self.storage[key]
+            self.dll.delete(node)
+            self.dll.add_to_head(value)
+            self.storage[key] = self.dll.head
+        elif self.limit == len(self.dll):
+            print("hit")
+            for v, k in self.storage.items():
+                if k.value == self.dll.tail.value:
+                    print("hit2")
+                    self.storage[v] = None
+            self.dll.remove_from_tail()
+            self.dll.add_to_head(value)
+            self.storage[key] = self.dll.head
+        else:
+            self.dll.add_to_head(value)
+            self.storage[key] = self.dll.head
+        # for i, k in self.storage.items():
+        #     print(i, k.value)
+
+
+cache = LRUCache(3)
+cache.set('item1', 'a')
+cache.set('item2', 'b')
+cache.set('item3', 'c')
+cache.get("item1")
+cache.set("item4", "d")
+cache.get("item1")
+cache.get("item3")
+cache.get("item4")
+print(cache.storage)
